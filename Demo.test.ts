8f03d445adceca9be5060e2efa78d8a04ed18b13
aleeeexcool <67423307+aleeeexcool@users.noreply.github.com>
2023-01-29 19:48:14 +0200

    Update Demo.test.ts

--- Demo.test.ts
+++ Demo.test.ts
@@ -3,17 +3,17 @@ import type { Demo } from "../typechain-types";
 
 describe("Demo", function() {
     async function deploy() {
-        const [ superadmin, withdrawer, user ] = await ethers.getSigners();
+        const [ superadmin, withdrawer, minter ] = await ethers.getSigners();
 
         const Factory = await ethers.getContractFactory("Demo");
-        const demo: Demo = await Factory.deploy(withdrawer.address);
+        const demo: Demo = await Factory.deploy(withdrawer.address, minter.address);
         await demo.deployed();
 
-        return { demo, withdrawer, superadmin }
+        return { demo, withdrawer, minter }
     }
 
-    it('works', async function() {
-        const { demo, withdrawer, superadmin } = await loadFixture(deploy);
+    it('wirhdraw works', async function() {
+        const { demo, withdrawer } = await loadFixture(deploy);
 
         const withdrawerRole = await demo.WITHDRAWER_ROLE();
         const defaultAdmin = await demo.DEFAULT_ADMIN_ROLE();
@@ -23,5 +23,18 @@ describe("Demo", function() {
         await demo.connect(withdrawer).withdraw();
 
         await expect(demo.withdraw()).to.be.revertedWith('no such role!');
+    });
+
+    it('that works', async function() {
+        const { demo, minter } = await loadFixture(deploy);
+
+        const minterRole = await demo.WITHDRAWER_ROLE();
+        const defaultAdmin = await demo.DEFAULT_ADMIN_ROLE();
+
+        expect(await demo.getRoleAdmin(minterRole)).to.eq(defaultAdmin);
+
+        await demo.connect(minter).getBalance();
+
+        await expect(demo.getBalance()).to.be.revertedWith('no such role!');
     });
 });
